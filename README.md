58c82e480c2b4884011f21839c1058c690e95ecd
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-17 21:32:20 -0700

    update the API w/ tests!

--- README.md
+++ README.md
@@ -2,13 +2,60 @@
 # spdy-push
 
 [![NPM version][npm-image]][npm-url]
-[![Latest tag][github-tag]][github-url]
 [![Build status][travis-image]][travis-url]
 [![Test coverage][coveralls-image]][coveralls-url]
 [![Dependency Status][david-image]][david-url]
 [![License][license-image]][license-url]
 [![Downloads][downloads-image]][downloads-url]
 [![Gittip][gittip-image]][gittip-url]
+
+A SPDY Push helper to be used with [spdy](https://github.com/indutny/node-spdy).
+
+- Handles `close` events and file descriptor leaks
+- Automatically gzips
+- Automatically sets the `content-length` and `content-type` headers if it can
+- Supports pushing strings, buffers, streams, and files
+
+## Example
+
+```js
+var spdy = require('spdy-push');
+
+require('spdy').createServer(require('spdy-keys'), function (req, res) {
+  if (res.isSpdy) {
+    spdy(res).push('/script.js', {
+      filename: 'public/script.js', // resolves against CWD
+    }).catch(function (err) {
+      console.error(err.stack); // log any critical errors
+    })
+  }
+
+  res.statusCode = 204;
+  res.end();
+})
+```
+
+## API
+
+### spdy(res).push([path], [options], [priority])
+
+- `path` is the path of the object being pushed.
+  Can also be set as `options.path`.
+- `priority` is the priority between `0-7` of the push stream
+  with `7`, the default, being the lowest priority.
+  Can also be set as `options.priority`.
+- `options` are:
+  - `headers`
+  - `body` - a `String`, `Buffer`, or `Stream.Readable` body
+  - `filename` - a path to a file. Resolves against CWD.
+
+Either `options.body` or `options.filename` must be set.
+
+You do not need to set the following headers:
+
+- `content-encoding`
+- `content-length`
+- `content-type`
 
 [npm-image]: https://img.shields.io/npm/v/spdy-push.svg?style=flat-square
 [npm-url]: https://npmjs.org/package/spdy-push
---
+++ test/test.js
@@ -0,0 +1,298 @@
+
+var spdy = require('spdy')
+var https = require('https')
+var zlib = require('mz/zlib')
+var assert = require('assert')
+var keys = require('spdy-keys')
+var join = require('path').join
+var Readable = require('stream').Readable
+var Promise = require('native-or-bluebird')
+
+var SPDY = require('..')
+
+var port
+var server
+var agent
+
+afterEach(function (done) {
+  agent.close()
+  server.close(done)
+})
+
+describe('Streams', function () {
+  describe('when empty', function () {
+    it('should push', function () {
+      return listen(function (req, res) {
+        var stream = new Readable()
+        stream.push(null)
+
+        return SPDY(res).push('/', {
+          body: stream
+        })
+      }).then(pull).then(function (res) {
+        res.resume()
+        return new Promise(function (resolve, reject) {
+          res.on('end', resolve)
+          res.on('error', reject)
+        })
+      })
+    })
+  })
+
+  describe('when text', function () {
+    it('should gzip', function () {
+      return listen(function (req, res) {
+        var stream = new Readable()
+        stream.push('klajsdlfjalsdkfjalsdjkfsjdf')
+        stream.push(null)
+
+        return SPDY(res).push('/', {
+          headers: {
+            'content-type': 'text/plain'
+          },
+          body: stream
+        })
+      }).then(pull).then(function (res) {
+        res.resume()
+        assert.equal(res.headers['content-encoding'], 'gzip')
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+        assert.equal(res.url, '/')
+      })
+    })
+  })
+
+  describe('when image', function () {
+    it('should not gzip', function () {
+      return listen(function (req, res) {
+        var stream = new Readable()
+        stream.push(null)
+
+        return SPDY(res).push('/', {
+          headers: {
+            'content-type': 'image/png'
+          },
+          body: stream
+        })
+      }).then(pull).then(function (res) {
+        res.resume()
+        assert(res.headers['content-encoding'] == null)
+        assert.equal(res.headers['content-type'], 'image/png')
+      })
+    })
+  })
+
+  describe('when svg', function () {
+    it('should push', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push('/fontawesome-webfont.svg', {
+          filename: join(__dirname, 'fontawesome-webfont.svg')
+        })
+      }).then(pull).then(function (res) {
+        res.resume()
+        assert.equal(res.headers['content-encoding', 'gzip'])
+        assert(~res.headers['content-type'].indexOf('image/svg+xml'))
+      })
+    })
+  })
+})
+
+describe('Strings', function () {
+  describe('when no content-type is set', function () {
+    it('should set the content-type if possible', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push('/some.txt', {
+          body: 'lol'
+        })
+      }).then(pull).then(function (res) {
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+      })
+    })
+  })
+
+  describe('when content-encoding is already set', function () {
+    it('should not compress', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push('/something.txt', {
+          body: 'klajsldkfjaklsdjflkajdsflkajsdlkfjaklsdjf',
+          threshold: 1,
+          headers: {
+            'content-encoding': 'identity'
+          }
+        })
+      }).then(pull).then(function (res) {
+        assert.equal(res.headers['content-encoding'], 'identity')
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+      })
+    })
+  })
+
+  describe('when empty', function () {
+    it('should push', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push('/some.txt', {
+          body: ''
+        })
+      }).then(pull).then(function (res) {
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+      })
+    })
+  })
+})
+
+describe('Buffers', function () {
+  describe('when already compressed', function () {
+    it('should not compress', function () {
+      return zlib.gzip('lol').then(function (body) {
+        listen(function (req, res) {
+          return SPDY(res).push({
+            path: '/',
+            threshold: 1,
+            headers: {
+              'content-encoding': 'gzip',
+              'content-type': 'text/plain',
+            },
+            body: body
+          })
+        })
+      }).then(pull).then(function (res) {
+        res.resume()
+        assert.equal(res.headers['content-encoding'], 'gzip')
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+      })
+    })
+  })
+
+  describe('when compressing', function () {
+    it('should remove the content-length', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push({
+          path: '/something.txt',
+          body: new Buffer(2048),
+          headers: {
+            'content-length': '2048'
+          }
+        })
+      }).then(pull).then(function (res) {
+        assert.equal(res.headers['content-encoding'], 'gzip')
+        assert(~res.headers['content-type'].indexOf('text/plain'))
+        assert(res.headers['content-length'] == null)
+      })
+    })
+  })
+})
+
+describe('Compression', function () {
+  describe('Thresholds', function () {
+    it('should not compress when below the threshold', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push({
+          path: '/something.txt',
+          body: 'lol',
+        })
+      }).then(pull).then(function (res) {
+        assert(!res.headers['content-encoding'])
+      })
+    })
+
+    it('should compress when above the threshold', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push({
+          path: '/something.txt',
+          body: 'lol',
+          threshold: 1
+        })
+      }).then(pull).then(function (res) {
+        assert.equal(res.headers['content-encoding'], 'gzip')
+      })
+    })
+  })
+
+  describe('.compress', function () {
+    it('should not compress when false', function () {
+      return listen(function (req, res) {
+        return SPDY(res).push({
+          path: '/something.txt',
+          body: 'lol',
+          threshold: 1,
+          compress: false
+        })
+      }).then(pull).then(function (res) {
+        assert(!res.headers['content-encoding'])
+      })
+    })
+  })
+})
+
+describe('Disconnections', function () {
+  it('should not leak file descriptors', function () {
+    var called = false
+    var stream = new Readable()
+    stream.destroy = function () {
+      called = true
+    }
+
+    return listen(function (req, res) {
+      return SPDY(res).push('/', {
+        body: stream
+      })
+    }).then(pull).then(function (res) {
+      res.destroy()
+
+      assert(called)
+    })
+  })
+})
+
+function listen(fn) {
+  return new Promise(function (resolve, reject) {
+    server = spdy.createServer(keys, function (req, res) {
+      var defer
+      try {
+        defer = fn(req, res)
+      } catch (err) {
+        console.error(err.stack)
+        res.statusCode = 500
+        res.end()
+        return
+      }
+
+      defer.then(function () {
+        res.statusCode = 204
+        res.end()
+      }).catch(function (err) {
+        console.error(err.stack)
+        res.statusCode = 500
+        res.end()
+      })
+    }).listen(port, function (err) {
+      if (err) return reject(err)
+      port = this.address().port
+      resolve()
+    })
+  })
+}
+
+function pull() {
+  return new Promise(function (resolve, reject) {
+    agent = spdy.createAgent({
+      port: port,
+      rejectUnauthorized: false,
+    })
+
+    agent.once('error', reject)
+    agent.once('push', resolve)
+
+    https.request({
+      agent: agent,
+      path: '/',
+    })
+    .once('error', reject)
+    .once('response', function (res) {
+      if (res.statusCode !== 204) reject(new Error('got status code: ' + res.statusCode))
+      res.resume()
+    })
+    .end()
+  })
+}
+
+function noop() {}
